5700522784edc4924edcfc3707fadd4cf0229a0b
Pawel Boening <pawel.boening@student.hpi.de>
2018-06-13 16:18:51 +0200

    added keras example

--- misc/test.py
+++ misc/test.py
@@ -1,6 +1,15 @@
-import random
+from keras.applications.resnet50 import ResNet50
+from keras.preprocessing import image
+from keras.applications.resnet50 import preprocess_input, decode_predictions
+import numpy as np
 
+model = ResNet50(weights='imagenet')
 
-for i in range(1000):
-    print(random.randint(0,10000))
+img_path = 'test.jpg'
+img = image.load_img(img_path, target_size=(224, 224))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
 
+preds = model.predict(x)
+print('Predicted:', decode_predictions(preds, top=3)[0])
