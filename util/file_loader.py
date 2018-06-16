f3edd9a67b93135fe4200821f4c33ec73fda64d9
Justin Trautmann <justin.trautmann@student.hpi.de>
2018-06-16 11:50:59 +0200

    output only raw caption list

--- util/file_loader.py
+++ util/file_loader.py
@@ -16,7 +16,6 @@ class File:
 class TrainFile(File):
     def __init__(self, path):
         super(TrainFile, self).__init__(path)
-        self.tokenizer = Tokenizer()
         self._id_file_map = None
 
     @property
@@ -27,10 +26,7 @@ class TrainFile(File):
                 self._id_file_map[annotation['id']] = annotation['file_name']
         return self._id_file_map
 
-    def encode_captions(self):
-        all_sentences = []
-        for annotation in self.data['annotations']:
-            all_sentences.append(annotation['caption'])
+    def captions(self):
+        return [annotation['caption'] for annotation in self.data['annotations']]
 
-        self.tokenizer.fit_on_texts(all_sentences)
-        return self.tokenizer.texts_to_sequences(all_sentences)
+        
