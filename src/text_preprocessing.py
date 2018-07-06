ec29ae380828e2e81dce2b12f1162c17a2194002
Leonard Marschke <leonard@marschke.me>
2018-07-06 19:52:13 +0200

    save work

--- src/text_preprocessing.py
+++ src/text_preprocessing.py
@@ -1,13 +1,5 @@
 import itertools
 import json
-from zipfile import ZipFile
-
-import shutil
-
-import os
-import requests
-
-from os import path
 from typing import List, Dict
 from itertools import groupby
 
@@ -19,78 +11,7 @@ from keras.optimizers import SGD
 from keras.preprocessing.text import Tokenizer
 
 from src.config import base_configuration
-
-
-class WordVector(object):
-    def vectorize_word(self, word: str):
-        return self.word_vectors[word]
-
-    def embedding_size(self):
-        return 300
-
-    def __init__(self, vocab, vector_type):
-        if vector_type not in base_configuration['files']['pretrained']['word_vectors']:
-            raise ValueError('Invalid vector type {}'.format(vector_type))
-
-        self._vector_type = vector_type
-        self._input_file = self.vector_type_info['path']
-        self._input_file = path.join(base_configuration['pretrained_models_path'], self._input_file)
-        self._input_file = self._input_file if path.isabs(self._input_file) else path.join('.', self._input_file)
-        self._fetch_data_if_needed()
-        self._load_pretrained_vectors(vocab)
-
-    @property
-    def vector_type_info(self):
-        return base_configuration['files']['pretrained']['word_vectors'][self._vector_type]
-
-    def _load_pretrained_vectors(self, vocab):
-        with open(self._input_file, "r", encoding="utf-8") as file:
-            if self._vector_type == 'fasttext':
-                next(file)  # Skip the first line as it is a header, this is format specific
-            self._load_pretrained_vectors_file(file, vocab)
-
-    def _fetch_data_if_needed(self):
-        if not path.exists(self._input_file):
-            os.makedirs(path.dirname(self._input_file), exist_ok=True)
-
-            uri_info = self.vector_type_info['uri']
-
-            if uri_info['type'] == 'file':
-                print("Downloading {} to {}".format(uri_info['url'], self._input_file))
-                response = requests.get(uri_info['url'], stream=True)
-                with open(self._input_file, 'wb') as file:
-                    shutil.copyfileobj(response.raw, file)
-                del response
-            elif uri_info['type'] == 'zip':
-                print("Downloading {} to {}.zip".format(uri_info['url'], self._input_file))
-                zip_file = self._input_file + '.zip'
-                response = requests.get(uri_info['url'], stream=True)
-                with open(zip_file, 'wb') as file:
-                    shutil.copyfileobj(response.raw, file)
-                del response
-
-                with ZipFile(zip_file, "r") as zip_ref:
-                    zip_ref.extractall(path.dirname(self._input_file))
-                os.unlink(zip_file)
-
-    def _load_pretrained_vectors_file(self, file_obj, vocab: Dict[str, int]):
-        self.word_vectors = {}
-        unsupported_lines = []
-
-        for line in file_obj:
-            tokens = line.split()
-            word = " ".join(tokens[:-300])
-            vector = tokens[-300:]
-            if word == '.':
-                self.word_vectors[TextPreprocessor.eos_token()] = np.asarray(vector, dtype='float32')
-            elif word in vocab:
-                if len(tokens) <= 300:
-                    unsupported_lines.append(line)
-                    continue
-                self.word_vectors[word] = np.asarray(vector, dtype='float32')
-        assert len(self.word_vectors) == len(vocab)
-        assert TextPreprocessor.eos_token() in self.word_vectors
-        assert len(unsupported_lines) <= 0, "{} unsupported lines\n".format(len(unsupported_lines)) + "\n".join(unsupported_lines)
+from src.word_vector import WordVector
 
 
 def word_embedding(vocab: Dict[str, int], word_vectors: WordVector):
@@ -194,15 +115,26 @@ class TextPreprocessor(object):
         self.vocab = self.tokenizer.word_index
 
     def encode_caption(self, caption):
-        return self.encode_captions([caption])
+        return self.encode_captions([caption])[0]
 
-    def encode_captions(self, captions: List[str]) -> np.ndarray:
+    def encode_captions(self, captions: List[str]) -> List[np.ndarray]:
+        """
+        Tokenizes and one-hot encodes captions. They are returned as numpy array with the shape
+        (num_captions, size_of_longest_caption, vocab_size + 1). Padding is encoded as zero-vector.
+        :param captions: list of the captions as string
+        :return: list of the one-hot encoded captions as numpy arrays
+        """
         captions_indices = self.tokenizer.texts_to_sequences(captions)
         captions_indices = [caption + [self.eos_token_index()] for caption in captions_indices]
         captions_indices = np.array(list(itertools.zip_longest(*captions_indices, fillvalue=0))).T
 
         max_idx = len(self.vocab) + 1
         return [self.one_hot_encode_caption(caption, max_idx) for caption in captions_indices]
+
+    def one_hot_encode_caption(self, caption_indices: List[int], one_hot_size: int) -> np.ndarray:
+        one_hot = np.zeros([len(caption_indices), one_hot_size])
+        one_hot[np.arange(len(caption_indices)), caption_indices] = 1
+        return np.pad(one_hot[:, 1:], [1, 0], mode='constant', constant_values=0)[1:]
 
     def decode_captions(self, one_hot_captions: np.ndarray) -> List[str]:
         """
