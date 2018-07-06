e0f5ac96f21d1129bb244aacc681cf0181c05a3f
Pawel Boening <pawel.boening@student.hpi.de>
2018-07-06 17:08:35 +0200

    fixed image net

--- src/image_net.py
+++ src/image_net.py
@@ -1,9 +1,12 @@
 import keras
 from keras import Sequential
 from keras.layers import Dense
+from keras.optimizers import SGD
+from keras.preprocessing.image import load_img, img_to_array
 
 from src.config import base_configuration
 from src.file_loader import File
+import numpy as np
 
 
 class ImageNet:
@@ -16,16 +19,32 @@ class ImageNet:
 
         layers.append(Dense(
             base_configuration['sizes']['rnn_input'],
-            input_shape=(299, 299, 3)
+            input_shape=[299 * 299 * 3]
         ))
 
         return layers
 
     @staticmethod
     def preprocess_image(path):
-        loaded_image = keras.preprocessing.image.load_img(path, (299, 299))
-        return keras.preprocessing.image.img_to_array(loaded_image)
+        loaded_image = load_img(path, (299, 299))
+        return np.reshape(img_to_array(loaded_image), [299 * 299 * 3])
 
     @property
     def images(self):
         return (self.preprocess_image(path) for path in self.file_loader.id_file_map.values())
+
+
+if __name__ == "__main__":
+    #file_loader = File.load(base_configuration['selected_dataset'])
+
+    image_net = ImageNet(None)
+
+    model = Sequential()
+    [model.add(layer) for layer in image_net.layers]
+
+
+    model.compile(loss="mean_squared_error", optimizer=SGD(lr=1e-4))
+
+    print(model.predict(np.random.normal(size=[1, 299 * 299 * 3])))
+    #print(model.predict(np.asarray([image_net.preprocess_image("")])))
+
