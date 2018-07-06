0b844be192d37d8826fcde232a8df75b87bc73e6
Leonard Marschke <leonard@marschke.me>
2018-07-06 21:32:32 +0200

    save work

--- training.py
+++ training.py
@@ -1,3 +1,5 @@
+from os import path
+
 from keras import Sequential
 from keras.layers import Dense
 import numpy as np
@@ -17,19 +19,16 @@ def model_list_add(model: Sequential, layer_list):
 
 def training_data(images, text_preprocessor, file_loader):
     batch_size = base_configuration['batch_size']
-    i = 0
     batch_images = []
     batch_captions = []
     for image_id, image in images:
         for caption in file_loader.id_caption_map[image_id]:
-            if i == batch_size:
+            if len(batch_images) >= batch_size:
                 yield (np.asarray(batch_images), np.asarray(batch_captions))
-                i = 0
                 batch_images = []
                 batch_captions = []
             batch_images.append(image)
             batch_captions.append(text_preprocessor.encode_caption(caption))
-            i += 1
 
 
 def main():
@@ -46,14 +45,17 @@ def main():
     model_list_add(model, rnn_net.layers)
     model.add(Dense(len(text_preprocessor.vocab) + 1))
 
-    #model = multi_gpu_model(model)
+    model = multi_gpu_model(model)
 
     model.compile(**base_configuration['model_hyper_params'])
 
     training_data_generator = training_data(image_net.images, text_preprocessor, file_loader)
-    model.fit_generator(training_data_generator, steps_per_epoch=image_net.images_num, **base_configuration['fit_params'])
 
-    model.save(path.join(base_configuration['tmp_path'], ''))
+    step_size = int((image_net.images_num / base_configuration['batch_size']) + .5)
+
+    model.fit_generator(training_data_generator, steps_per_epoch=step_size, **base_configuration['fit_params'])
+
+    model.save(path.join(base_configuration['tmp_path'], 'model-all.hdf5'), overwrite=True)
 
 
 if __name__ == '__main__':
