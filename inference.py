95f8213eeb025064075f32d76a2df794cf07f5d2
Pawel Boening <pawel.boening@student.hpi.de>
2018-07-07 10:46:18 +0200

    loading images for inference

--- inference.py
+++ inference.py
@@ -4,17 +4,32 @@ import numpy as np
 from keras.models import load_model
 
 from src.config import base_configuration
+from src.file_loader import File
+from src.image_net import ImageNet
 
+
+def prediction_data(images, file_loader):
+    batch_size = base_configuration['batch_size']
+    batch_images = []
+    for image_id, image in images:
+        if len(batch_images) >= batch_size:
+            yield np.asarray(batch_images)
+            batch_images = []
+        batch_images.append(image)
 
 def main():
     model_dir = path.join(base_configuration['tmp_path'], 'model-saves')
     model_epoch = 5
     model_path = path.join(model_dir, '{:02d}.hdf5'.format(model_epoch))
 
-    model = load_model('model.h5')
+    model = load_model(model_path)
 
-    samples = [np.array(42)]
-    model.predict(samples)
+    file_loader = File.load(base_configuration['selected_dataset'])
+    image_net = ImageNet(file_loader)
+
+    prediction_data_generator = prediction_data(image_net.images, file_loader)
+
+    model.predict_generator(prediction_data_generator)
 
 
 if __name__ == '__main__':
