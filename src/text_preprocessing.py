2e7b170726374e48535119ab0d94a36b101e48bc
Jan Ehmueller <jan.ehmueller@student.hpi.de>
2018-07-07 15:18:04 +0200

    fix test in textpreprocessor

--- src/text_preprocessing.py
+++ src/text_preprocessing.py
@@ -175,7 +175,7 @@ if __name__ == "__main__":
     file_loader = File.load(base_configuration['selected_dataset'])
     captions = file_loader.captions()
     tp = TextPreprocessor()
-    tp.process_captions(captions)
+    tp.process_captions([captions])
     [print(tp.decode_caption(tp.encode_caption(cap))) for cap in captions[:10]]
 
         # model = Sequential()
