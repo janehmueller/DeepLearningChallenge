3d86cd8b283f81b4314c72684b22a299f40664d2
Jan Ehmueller <jan.ehmueller@student.hpi.de>
2018-07-08 13:13:28 +0200

    Fix gpu check

--- training.py
+++ training.py
@@ -83,7 +83,7 @@ def main():
         **base_configuration['model_hyper_params']
     )
 
-    if onGPU and countGPU is not None:
+    if onGPU and countGPU is None:
         model = multi_gpu_model(model)
     model.compile(loss=categorical_crossentropy_from_logits, **base_configuration['model_hyper_params'])
 
