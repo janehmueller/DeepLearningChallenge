b6da447e359c5a45dd1226b354c06d2093e2be82
Pawel Boening <pawel.boening@student.hpi.de>
2018-07-08 16:12:14 +0200

    remove error

--- inference.py
+++ inference.py
@@ -67,9 +67,9 @@ def main():
     predictions = predict(model, prediction_data_generator, step_size, text_preprocessor)
 
     for i, prediction in enumerate(predictions):
-        print(file_loader.id_file_map[processed_images[i]])
+        #print(file_loader.id_file_map[processed_images[i]])
         print(prediction)
-        print(file_loader.id_caption_map[processed_images[i]])
+        #print(file_loader.id_caption_map[processed_images[i]])
 
     # predictions = model.predict_generator(prediction_data_generator, steps=1)
     # captions_indices = [np.argmax(pred_caption, axis=1) for pred_caption in predictions]
