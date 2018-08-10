5a3c70357607b9b888bb1a5bc07cc553a6085170
Leonard Marschke <leonard@marschke.me>
2018-08-10 16:18:18 +0200

    allow use of multiple GPUs

--- util/checkGPU.py
+++ util/checkGPU.py
@@ -2,6 +2,11 @@ import os
 
 from keras.backend.tensorflow_backend import _is_current_explicit_device, _get_available_gpus
 
-onGPU = not _is_current_explicit_device('CPU') and len(_get_available_gpus()) > 0
+on_GPU = not _is_current_explicit_device('CPU') and len(_get_available_gpus()) > 0
 
-countGPU = os.environ.get('CUDA_VISIBLE_DEVICES', None)
+gpu_list = os.environ.get('CUDA_VISIBLE_DEVICES', None)
+
+if gpu_list:
+    gpu_list = gpu_list.split(',')
+else:
+    gpu_list = []
