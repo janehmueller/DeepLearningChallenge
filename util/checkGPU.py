094d4b3a8efff4bb732f07a86cbf788ed27e9875
Jan Ehmueller <jan.ehmueller@student.hpi.de>
2018-08-10 17:45:26 +0200

    fix gpu list indies

--- util/checkGPU.py
+++ util/checkGPU.py
@@ -7,6 +7,6 @@ on_GPU = not _is_current_explicit_device('CPU') and len(_get_available_gpus()) >
 gpu_list = os.environ.get('CUDA_VISIBLE_DEVICES', None)
 
 if gpu_list:
-    gpu_list = [int(gpu_id) for gpu_id in gpu_list.split(',')]
+    gpu_list = range(0, len(gpu_list))
 else:
     gpu_list = []
